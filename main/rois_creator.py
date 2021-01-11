0b81f070c8b0ea08d99630526f3fac177d03e133
irenebritney <irenebritney@192.168.31.84>
2021-01-11 17:54:33 +0800

    + main path

--- main/rois_creator.py
+++ main/rois_creator.py
@@ -5,7 +5,13 @@
 @author: luoyi
 Created on 2021年1月5日
 '''
-import main.main_path
+import sys
+import os
+#    取项目根目录
+ROOT_PATH = os.path.abspath(os.path.dirname(__file__)).split('faster_rcnn')[0]
+ROOT_PATH = ROOT_PATH + "faster_rcnn"
+sys.path.append(ROOT_PATH)
+
 
 import data.dataset_rois as rois
 import utils.conf as conf
