fde5869554af9acdafcd9a00326c4bc6961368c8
irenebritney <irenebritney@192.168.31.84>
2021-02-01 01:26:21 +0800

    + 改变roi_algin实现方式：先crop_and_resize，再avg_pooling

--- models/layers/pooling/preprocess.py
+++ models/layers/pooling/preprocess.py
@@ -22,22 +22,22 @@ def roi_align(fmaps, y_true, roipooling_ksize=[7, 7]):
     #    取必要的参数
     H, W, _ = fmaps.shape[1], fmaps.shape[2], fmaps.shape[3]                #    特征图宽高
     B, num = y_true.shape[0], y_true.shape[1]                               #    batch_size, 每个batch_size中的proposal数
-#     crop_size = (roipooling_ksize[0] * 2, roipooling_ksize[1] * 2)
-    crop_size = roipooling_ksize
+    crop_size = (roipooling_ksize[0] * 2, roipooling_ksize[1] * 2)
+#     crop_size = roipooling_ksize
     
     #    将特征图统一切成roipooling_ksize * 2的大小
-    xl = y_true[:,:,1] / W
-    yl = y_true[:,:,2] / H 
-    xr = (y_true[:,:,3] + 1) / W                                          #    +1是为了包含右下边界。参考：4*4特征图时，切[0,0, 3,3]最终算出来是0-0.75的区域
-    yr = (y_true[:,:,4] + 1) / H                                          #    +1是为了包含右下边界
-    boxes = tf.stack([yl,xl, yr,xr], axis=-1)                             #    boxes参数的顺序是ymin,xmin, ymax,xmax，且必须为0~1之间
+    xl = y_true[:,:,1] / (W - 1)
+    yl = y_true[:,:,2] / (H - 1) 
+    xr = (y_true[:,:,3]) / (W - 1)                                          #    -1是为了让W,H从0开始
+    yr = (y_true[:,:,4]) / (H - 1)                                          
+    boxes = tf.stack([yl,xl, yr,xr], axis=-1)                               #    boxes参数的顺序是ymin,xmin, ymax,xmax，且必须为0~1之间
     binx = tf.expand_dims(tf.range(B, dtype=tf.int32), axis=0)
     binx = tf.repeat(binx, repeats=num, axis=1)
     binx = tf.squeeze(binx)
     boxes = tf.reshape(boxes, shape=(B*num, 4))
     #    抠图，并用双线性插值reshape
     crops = tf.image.crop_and_resize(fmaps, boxes=boxes, box_indices=binx, crop_size=crop_size)
-#     crops = tf.nn.max_pool(crops, ksize=[2,2], strides=2, padding='VALID')
+    crops = tf.nn.avg_pool(crops, ksize=[2,2], strides=2, padding='VALID')
     return crops
 
 
